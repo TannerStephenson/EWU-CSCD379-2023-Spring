98038eaa5a524342a8d434f1a5d013ee7cdc7c0f
TannerStephenson <117472530+TannerStephenson@users.noreply.github.com>
2023-05-31 19:22:10 -0700

    Think it looks good?

--- Wordle.Api/Wordle.Api/Data/Seeder.cs
+++ Wordle.Api/Wordle.Api/Data/Seeder.cs
@@ -27,17 +27,18 @@
             }
         }
 
-        public static void SeedPlayers (AppDbContext db)
+        [HttpGet]
+        public async Task<CardDto> Get()
         {
-            if (!db.Players.Any())
+            var randomCard = await _cardService.GetRandomCardAsync();
+            // Map the Card object to a CardDto object
+            var cardDto = new CardDto
             {
-                db.Players.Add(new Player { Name = "Sam", AverageAttempts = 4.5, AverageSecondsPerGame = 62, GameCount = 25, PlayerId = Guid.NewGuid() });
-                db.Players.Add(new Player { Name = "James", AverageAttempts = 4.8, AverageSecondsPerGame = 195, GameCount = 42, PlayerId = Guid.NewGuid() });
-                db.Players.Add(new Player { Name = "Kelly", AverageAttempts = 3.5, AverageSecondsPerGame = 30, GameCount = 55, PlayerId = Guid.NewGuid() });
-                db.Players.Add(new Player { Name = "Susan", AverageAttempts = 2.7, AverageSecondsPerGame = 70, GameCount = 23, PlayerId = Guid.NewGuid() });
-                db.SaveChanges();
-            }
+                Suit = randomCard.Suit,
+                CardValue = randomCard.CardValue
+            };
+            return cardDto;
         }
 
-   }
+    }
 }
